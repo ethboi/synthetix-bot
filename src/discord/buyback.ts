f71051f46d5026f97f46ae12a75a4fb2ae7a0779
MochaByte <mochabyte@gmx.net>
2024-01-29 11:01:18 +0100

    update: show totalburned snx as well

--- src/discord/buyback.ts
+++ src/discord/buyback.ts
@@ -1,51 +1,65 @@
-import { Client, ActivityType } from 'discord.js'
-import { displayNumber } from '../utils/formatNumber'
-import { GetBuybackData } from '../actions/buyback'
-import { Buyback } from '../types/buyback'
+
+// Import necessary libraries
+import { Client, ActivityType } from 'discord.js';
+import { displayNumber } from '../utils/formatNumber';
+import { GetBuybackData } from '../actions/buyback'; 
+import { Buyback } from '../types/buyback'; 
 
 export async function SetUpDiscordBuyback(discordClient: Client, accessToken: string, frontEnd: string) {
   discordClient.on('ready', async () => {
-    console.debug(`Discord Buyback/Burn bot is online!`)
-    const buyback = await GetBuybackData()
-    await setNameActivityBuyback(discordClient, buyback)
-  })
-  await discordClient.login(accessToken)
-  return discordClient
+    console.debug(`Discord Buyback/Burn bot is online!`);
+    const buyback = await GetBuybackData();
+    await setNameActivityBuyback(discordClient, buyback);
+  });
+  await discordClient.login(accessToken);
+  return discordClient;
 }
 
 export async function setNameActivityBuyback(client: Client, buyback: Buyback) {
   try {
-    const weeklyBurnedSNX = await calculateWeeklyBurnedSNX()
-    const username = `${displayNumber(weeklyBurnedSNX)} SNX`
-    const now = new Date()
-    const previousWednesday = getPreviousWednesday(now)
-    const startDateString = previousWednesday.toLocaleDateString('en-US', { month: 'short', day: 'numeric' })
-    const endDateString = now.toLocaleDateString('en-US', { month: 'short', day: 'numeric' })
+    const weeklyBurnedSNX = await calculateWeeklyBurnedSNX();
+    const totalBurnedSNX = await calculateTotalBurnedSNX();
+    const username = `${displayNumber(weeklyBurnedSNX)} / ${displayNumber(totalBurnedSNX)} SNX`; 
+    const now = new Date();
+    const previousWednesday = getPreviousWednesday(now);
+    const startDateString = previousWednesday.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+    const endDateString = now.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
     // If you want to show the timeframe uncomment below:
     // const activity = `Weekly Wednessay Burn: ${startDateString} - ${endDateString}`;
-    const activity = `Weekly Burn`
+    const activity = `Weekly / Total Burn`;
+
+    console.log('Weekly Buyback/Burn');
+    console.log(username);
+    console.log(activity);
 
     client.guilds.cache.map(
       async (guild) => await guild.members.cache.find((m) => m.id == client.user?.id)?.setNickname(username),
-    )
+    );
     client.user?.setActivity(activity, {
       type: ActivityType.Watching,
-    })
+    });
   } catch (e: any) {
-    console.log(e)
+    console.log(e);
   }
 }
 
 async function calculateWeeklyBurnedSNX(): Promise<number> {
-  const buyback = await GetBuybackData()
-  return buyback.weeklyBurnedSNX
+  const buyback = await GetBuybackData();
+  return buyback.weeklyBurnedSNX;
+}
+
+async function calculateTotalBurnedSNX(): Promise<number> {
+  const buyback = await GetBuybackData();
+  return buyback.totalBurnedSNX;
 }
 
 function getPreviousWednesday(now: Date): Date {
-  const dayOfWeek = now.getDay()
-  const daysSincePreviousWednesday = (dayOfWeek + 7 - 3) % 7
-  const previousWednesday = new Date(now)
-  previousWednesday.setDate(now.getDate() - daysSincePreviousWednesday)
-  previousWednesday.setHours(0, 0, 0, 0) // Set hours to midnight
-  return previousWednesday
+  const dayOfWeek = now.getDay();
+  const daysSincePreviousWednesday = (dayOfWeek + 7 - 3) % 7;
+  const previousWednesday = new Date(now);
+  previousWednesday.setDate(now.getDate() - daysSincePreviousWednesday);
+  previousWednesday.setHours(0, 0, 0, 0); // Set hours to midnight
+  return previousWednesday;
 }
+
+
